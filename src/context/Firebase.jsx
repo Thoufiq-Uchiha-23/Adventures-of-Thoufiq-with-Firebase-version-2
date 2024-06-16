f803b6d49ceab6b9173ba9ed1e8cd10956029153
Thoufiq-Izhar-23 <thoufiqizhar022@gmail.com>
2024-06-17 00:01:46 +0530

    setting up database

--- src/context/Firebase.jsx
+++ src/context/Firebase.jsx
@@ -1,10 +1,32 @@
 import { createContext } from "react";
+import { initializeApp } from "firebase/app";
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth"
+import { getDatabase } from "firebase/database";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCBqBZM7BEwhOxYeiVGF2O50d__kUiGqMY",
+    authDomain: "app-e0f8e.firebaseapp.com",
+    databaseURL: "https://app-e0f8e-default-rtdb.firebaseio.com",
+    projectId: "app-e0f8e",
+    storageBucket: "app-e0f8e.appspot.com",
+    messagingSenderId: "639593789333",
+    appId: "1:639593789333:web:69cb460967a54ca4fae89c",
+    databaseURL: "https://app-e0f8e-default-rtdb.firebaseio.com"
+};
+
+const firebaseApp = initializeApp(firebaseConfig)
+const firebaseAuth = getAuth(firebaseApp)
+const database = getDatabase()
 
 const FirebaseContext = createContext(null)
 
+
 export const FirebaseProvider = (props) => {
+    const signUserWithEmailAndPassword = (email, password) => {
+        return createUserWithEmailAndPassword(firebaseAuth, email, password)
+    }
     return (
-        <FirebaseContext.Provider>
+        <FirebaseContext.Provider value={signUserWithEmailAndPassword}>
             {props.children}
         </FirebaseContext.Provider>
     )
