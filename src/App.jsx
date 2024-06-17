05fc80beeac42dd33fb8f7be9e05e632baa0e0f5
Thoufiq-Izhar-23 <thoufiqizhar022@gmail.com>
2024-06-18 00:17:46 +0530

    error

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,28 @@
+import { useState } from 'react';
 import { useFirebase } from './context/Firebase'
+import {signupUserWithEmailAndPassword} from 'firebase/auth' 
 import './App.css'
 
 function App() {
 
   const firebase = useFirebase()
+  const [email, setEmail] =useState('')
+  const [password, setPassword] =useState('')
 
   console.log("Firebase", firebase);
 
   return (
     <div>
       <h1>Firebase</h1>
-      <input type="email" placeholder='Enter Email' />
-      <input type="password" placeholder='Enter Password' />
-      <button onClick={() => {}}>Signup</button>
+      <input
+      onChange={(e) => setEmail(e.target.value)}
+      value={email} 
+      type="email" placeholder='Enter Email' />
+      <input
+      onChange={(e) => setPassword(e.target.value)}
+      value={password} 
+      type="password" placeholder='Enter Password' />
+      <button onClick={() => firebase.signupUserWithEmailAndPassword}>Signup</button>
     </div>
   )
 }
