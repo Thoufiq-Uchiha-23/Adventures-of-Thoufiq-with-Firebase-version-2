0e2a7d5db61a49a01453efdbb9f7a33201551564
Thoufiq-Izhar-23 <thoufiqizhar022@gmail.com>
2024-06-17 23:54:26 +0530

    Setting up firebase using context api

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useFirebase } from './context/Firebase'
 import './App.css'
 
 function App() {
 
+  const firebase = useFirebase()
+
+  console.log("Firebase", firebase);
+
   return (
-    <div><h1>Firebase</h1></div>
+    <div>
+      <h1>Firebase</h1>
+      <input type="email" placeholder='Enter Email' />
+      <input type="password" placeholder='Enter Password' />
+      <button onClick={() => {}}>Signup</button>
+    </div>
   )
 }
 
